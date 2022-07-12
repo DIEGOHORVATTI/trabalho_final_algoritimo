18f5e791812825c3b3c35cd1f19c87018525651f
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-11 20:41:51 -0400

    fix: matriz radomica de DNA atribuida com sucesso

--- controller/fill_matriz.c
+++ controller/fill_matriz.c
@@ -7,60 +7,74 @@
   a+ -> leitura e escrita (adiciona ao final do arquivo)
 */
 
+char dnaLetra(){
+
+  struct timeval tv; // tv -> timeval
+  gettimeofday(&tv, 0);
+
+  long int timeMilesegundos = tv.tv_usec * getpid(); // precisão de milesegundos
+  srand(timeMilesegundos);
+
+  char string[] = {'A', 'T', 'C', 'G'};
+
+  // 1 -> A
+  // 2 -> T
+  // 3 -> C
+  // 4 -> G
+
+  return (string[(rand() % 4)]);
+}
+
 void fillMatriz(){
 
-  srand(time(NULL)); //seed
+    s_configuration config = Config();
 
-  s_configuration config = Config();
-  
-  FILE *arquivo = fopen("./matriz.txt", "w+");
+    FILE *arquivo = fopen("./matriz.txt", "w+");
 
-  // matriz dinamica
-    // aloca um vetor de config.LINHAS ponteiros para linhas
-    int **matriz;
-    matriz = malloc(config.LINHAS * sizeof(int *));
+    // matriz dinamica
+      // aloca um vetor de config.LINHAS ponteiros para linhas
+      char **matriz;
+      matriz = malloc(config.LINHAS * sizeof(char*));
 
-    // aloca cada uma das linhas (vetores de config.COLUNAS inteiros)
-    for (int i = 0; i < config.LINHAS; i++){
-      matriz[i] = malloc(config.COLUNAS * sizeof(int));
-    }
-  
-  //verifica de matriz foi alocada
-  if(matriz != NULL){
-    printf("\n%s Matriz alocada com sucesso%s\n", __COLOR_BLUE, __COLOR_FIM);
-  
-    // verificar se arquivo existe
-    if (arquivo != NULL){
-
-      printf("\n%s Arquivo 'matriz.txt' encontrado%s\n\n", __COLOR_BLUE, __COLOR_FIM);
-
-      // atribuir valores aleatórios a matriz (A, T, C, G)
-      for (int j = 0; j < config.COLUNAS; j++){
-        for (int i = 0; i < config.LINHAS; i++){
-          *(*(matriz + i) + j) = 10+(rand() % 90);
-        }
-      }
-      
-      // printar matriz
-      for (int j = 0; j < config.COLUNAS; j++){
-        for (int i = 0; i < config.LINHAS; i++){
-          //printf(" %d", mat[j][i]);
-          printf(" %d", *(*(matriz+i) + j) );
-        }
-        printf("\n");
-      }
-
-      // libera a memória da matriz
+      // aloca cada uma das linhas (vetores de config.COLUNAS inteiros)
       for (int i = 0; i < config.LINHAS; i++){
-        free(matriz[i]);
+        matriz[i] = malloc(config.COLUNAS * sizeof(char));
       }
-      free(matriz);
 
-    }else{
-      printf("\n%s Erro ao abrir o arquivo 'matriz.txt'%s\n", __COLOR_RED, __COLOR_FIM );
-    }
-  }
+    //verifica de matriz foi alocada
+    if(matriz != NULL){
+      //#printf("\n%s Matriz alocada com sucesso%s\n", __COLOR_BLUE, __COLOR_FIM);
 
-  fclose(arquivo);
+      // verificar se arquivo existe
+      if (arquivo != NULL){
+        //#printf("\n%s Arquivo 'matriz.txt' encontrado%s\n\n", __COLOR_BLUE, __COLOR_FIM);
 
-} 
+        // atribuir valores aleatórios a matriz [A, T, C, G]
+        for (int j = 0; j < config.COLUNAS; j++){
+          for (int i = 0; i < config.LINHAS; i++){
+            *(*(matriz + i) + j) = dnaLetra();
+          }
+        }
+
+        // atribuir matriz ao arquivo
+        for (int j = 0; j < config.COLUNAS; j++){
+          for (int i = 0; i < config.LINHAS; i++){
+            fputc( *(*(matriz + i) + j), arquivo); // letra
+            fputc('\t', arquivo);                  // espaço
+          }
+          if (j < config.COLUNAS - 1){
+            fputc('\n', arquivo);                  // quebra de linha
+          }
+        }
+
+        // libera a memória da matriz
+        for (int i = 0; i < config.LINHAS; i++){
+          free(matriz[i]);
+        }
+        free(matriz);
+
+      }else{ printf("\n%s Erro ao abrir o arquivo 'matriz.txt'%s\n", __COLOR_RED, __COLOR_FIM ); }
+    }else{ printf("\n%s Erro ao alocar matriz dinamica 'matriz' %s\n", __COLOR_RED, __COLOR_FIM ); }
+
+    fclose(arquivo);
+}
--- src/header.h
+++ src/header.h
@@ -1,8 +1,8 @@
-#include <stdio.h>          //input, output
-#include <stdlib.h>         //tools
-#include <string.h>         //tools for string
-#include <time.h>           //system date and time
-#include "../src/colors.h"  //cores globais
-
-// lib read file .conf     
-#include "../API/inih.c"
+#include <stdio.h>          // input, output
+#include <stdlib.h>         // tools
+#include <string.h>         // tools for string
+#include <time.h>           // system date and time
+#include <sys/time.h>       // system microseconds
+#include <unistd.h>         // operating system API
+#include "../src/colors.h"  // cores globais
+#include "../API/inih.c"    // lib read file .conf
