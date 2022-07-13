d9b00a0c37e186e11704c1bb7572d6bcc0b9020b
DIEGOHORVATTI <d.horvattid@gmail.com>
2022-07-13 19:26:35 -0400

    [13/07/2022] [19:26] -- {194, quarta}

--- controller/isSimian.c
+++ controller/isSimian.c
@@ -1,54 +1,168 @@
+char* A(){
+  //[A]
+  s_configuration config = Config();
+  char *A = calloc(config.OCORRENCIA, sizeof(char));
+
+  for (int i = 0; i < config.OCORRENCIA; i++) *(A + i) = 'A';
+
+  return(A);
+  free(A);
+}
+
+char* T(){
+  //[T]
+  s_configuration config = Config();
+  char *T = calloc(config.OCORRENCIA, sizeof(char));
+
+  for (int i = 0; i < config.OCORRENCIA; i++) *(T + i) = 'T';
+
+  return(T);
+  free(T);
+}
+
+char* C(){
+  //[C]
+  s_configuration config = Config();
+  char *C = calloc(config.OCORRENCIA, sizeof(char));
+
+  for (int i = 0; i < config.OCORRENCIA; i++) *(C + i) = 'C';
+
+  return(C);
+  free(C);
+}
+
+char* G(){
+  //[G]
+  s_configuration config = Config();
+  char *G = calloc(config.OCORRENCIA, sizeof(char));
+
+  for (int i = 0; i < config.OCORRENCIA; i++) *(G + i) = 'G';
+
+  return(G);
+  free(G);
+}
+
+
+int verificaHorizontal(char *stringPercorrido){
+
+  char *horizontalA = strstr(stringPercorrido, A());
+  if( horizontalA ){
+    printf(" ocorrencia horizontal: %s\n", A());
+    return(1);
+  }
+
+    char *horizontalT = strstr(stringPercorrido, T());
+  if( horizontalT ){
+    printf(" ocorrencia horizontal: %s\n", T());
+    return(1);
+  }
+
+  char *horizontalC = strstr(stringPercorrido, C());
+  if( horizontalC ){
+    printf(" ocorrencia horizontal: %s\n", C());
+    return(1);
+  }
+
+  char *horizontalG = strstr(stringPercorrido, G());
+  if( horizontalG ){
+    printf(" ocorrencia horizontal: %s\n", G());
+    return(1);
+  }
+}
+
+int verificaVertical(char *stringPercorrido){
+
+  char *horizontalA = strstr(stringPercorrido, A());
+  if( horizontalA ){
+    printf(" ocorrencia horizontal: %s\n", A());
+    return(1);
+  }
+
+    char *horizontalT = strstr(stringPercorrido, T());
+  if( horizontalT ){
+    printf(" ocorrencia horizontal: %s\n", T());
+    return(1);
+  }
+
+  char *horizontalC = strstr(stringPercorrido, C());
+  if( horizontalC ){
+    printf(" ocorrencia horizontal: %s\n", C());
+    return(1);
+  }
+
+  char *horizontalG = strstr(stringPercorrido, G());
+  if( horizontalG ){
+    printf(" ocorrencia horizontal: %s\n", G());
+    return(1);
+  }
+}
+
+
+
 int isSimian(char *dna){
 
   s_configuration config = Config();
 
-  //printar vetor
-  /* 
-    for (int i=0; i < (config.LINHAS * config.COLUNAS); i++){
-      printf("%c", dna[i]);
-    } 
-  */
+  //matriz vetor de vetorescom ponteiros
+  char **matriz;
+  matriz = malloc(config.LINHAS * sizeof(char*));
 
-  //[A, T, C, G]
-  char *A = calloc( config.OCORRENCIA, sizeof(char) );
-  char *T = calloc( config.OCORRENCIA, sizeof(char) );
-  char *C = calloc( config.OCORRENCIA, sizeof(char) );
-  char *G = calloc( config.OCORRENCIA, sizeof(char) );
-
-  for (int i = 0; i < config.OCORRENCIA; i++){
-    *(A + i) = 'A';
-    *(T + i) = 'T';
-    *(C + i) = 'C';
-    *(G + i) = 'G';
+  // aloca cada uma das linhas (vetores de config.COLUNAS inteiros)
+  for (int i = 0; i < config.LINHAS; i++){
+    matriz[i] = malloc(config.COLUNAS * sizeof(char));
   }
+  
+  // atualiza matriz
+  if(matriz){
+    int y=0;
+    for(int j = 0; j < config.COLUNAS ; j++){
+      for(int i = 0; i < config.LINHAS ; i++){
+        *(*(matriz + i) + j) = *(dna+y);
+        y++;
+      }
+    }
+    
+    // printa matriz 
+    for(int j = 0; j < config.COLUNAS ; j++){
+      for(int i = 0; i < config.LINHAS ; i++){
+        printf(" %c", *(*(matriz + i) + j) );
+      }printf("\n");
+    }
+    
+  }else{ printf("\n%s Erro ao alocar matriz dinamica 'matriz' %s\n", __COLOR_RED, __COLOR_FIM ); }
 
-  char *A_vertical_horizontal = strstr(dna, A);
-  if(A_vertical_horizontal){
-    printf(" %s\n", A);
-    return(1); 
+
+  for(int j = 0; j < config.COLUNAS; j++){
+    for(int i = 0; i < config.LINHAS; i++){
+
+      // alocação do vetor da matriz
+      char *stringPercorrido = calloc( config.LINHAS, sizeof(char));
+
+      if(stringPercorrido){
+        
+        // atualizar vetor com linha da matriz
+        for (int y = 0; y < config.LINHAS; y++){
+          *(stringPercorrido + y) = *(*(matriz + y) + i);
+        }
+        
+        
+        if ( verificaHorizontal(stringPercorrido) != 0){
+          //if ( verificaVertical(stringPercorrido) != 0){
+          //}
+        }
+       
+
+        free(stringPercorrido);
+      }else{ printf("\n%s Erro ao alocar matriz dinamica 'matriz' %s\n", __COLOR_RED, __COLOR_FIM ); }
+
+    }
   }
-
-  char *T_vertical_horizontal = strstr(dna, T);
-  if(T_vertical_horizontal){ 
-    printf(" %s\n", T);
-    return(1); 
+  
+  // libera a memória da matriz
+  for (int i = 0; i < config.LINHAS; i++){
+    free(matriz[i]);
   }
+  free(matriz);
 
-  char *C_vertical_horizontal = strstr(dna, C);
-  if(C_vertical_horizontal){
-    printf(" %s\n", C);
-    return(1); 
-  }
-
-  char *G_vertical_horizontal = strstr(dna, G);
-  if(G_vertical_horizontal){
-    printf(" %s\n", G);
-    return(1); 
-  }
-
-  free(A);
-  free(T);
-  free(C);
-  free(G);
   return(0);
 }
--- relatorio.md
+++ relatorio.md
@@ -1,11 +1,12 @@
-* Tentativa de padrão de projeto orientado a MVC
-* Arquivo de config.ini para guardar informações que o usuário pode facilmente alterar
+* Padrão de projeto MVC 
+* Arquivo config.ini para guardar informações que o usuário pode facilmente alterar
 * Reader e compilação assíncrona compartilhado com todos os arquivos.c
 * Verificar se arquivo de config.ini e arquivo.txt existem e estão legíveis
-* Converter encontrar e aplicar lógica da API inih a types de dados do arquivo config.ini conforme os types setados no model
+* Converter e aplicar lógica da API inih a types de dados do arquivo config.ini conforme os types setados no model
 * Criar uma função que retorna Y T C G randomicamente usando miles segundos do sistema(DNA)
 * Atribuir função DNA a matriz principal
 * Atribuir matriz a arquivo
 * Atribuir matriz de arquivo a uma string 
 * Verificar ocorrências de símios pelo vetor de matriz do arquivo 
 * Printar informações
+* pedir para printar novamente
